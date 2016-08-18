5950c741512cdeac1cd323fed6de31f8ba88d996
Christian Rondeau <christian.rondeau@gmail.com>
2016-08-18 09:25:56 -0400

    8 -> 2,2,2

--- spec/primes-spec.js
+++ spec/primes-spec.js
@@ -5,8 +5,16 @@ describe("primes generator", function() {
 	it("2 -> 2", function() {
 		expect(primes(2)).toEqual([2]);
 	});
-				
+
 	it("4 -> 2,2", function() {
 		expect(primes(4)).toEqual([2,2]);
+	});
+
+	it("6 -> 2,3", function() {
+		expect(primes(6)).toEqual([2,3]);
+	});
+
+	it("8 -> 2,2,2", function() {
+		expect(primes(8)).toEqual([2,2,2]);
 	});
 });
--- src/primes.js
+++ src/primes.js
@@ -1,7 +1,10 @@
 module.exports = function(n) {
-	if(n === 4) {
-		return [2,2];
-	} else {
-		return [2];
+  var d = 2;	
+	var r = [];
+	while(n > 2 && n % 2 === 0) {
+		n = n / 2;
+		r.push(2);
 	}
+	r.push(n);
+	return r;
 }
