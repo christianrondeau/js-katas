75c02ddf842e73c1e0883ca190a160c77ab0a21c
Christian Rondeau <christian.rondeau@gmail.com>
2016-08-20 22:27:50 -0400

    Can now do numbers up to XIII

--- roman/spec/roman-spec.js
+++ roman/spec/roman-spec.js
@@ -5,4 +5,32 @@ describe("roman numbers generator", function() {
 	it("1 -> I", function() {
 		expect(roman(1)).toEqual('I');
 	});
+
+	it("2 -> II", function() {
+		expect(roman(2)).toEqual('II');
+	});
+
+	it("5 -> V", function() {
+		expect(roman(5)).toEqual('V');
+	});
+
+	it("4 -> IV", function() {
+		expect(roman(4)).toEqual('IV');
+	});
+
+	it("10 -> X", function() {
+		expect(roman(10)).toEqual('X');
+	});
+
+	it("9 -> IX", function() {
+		expect(roman(9)).toEqual('IX');
+	});
+
+	it("6 -> VI", function() {
+		expect(roman(6)).toEqual('VI');
+	});
+
+	it("7 -> VII", function() {
+		expect(roman(7)).toEqual('VII');
+	});
 });
--- roman/src/roman.js
+++ roman/src/roman.js
@@ -1,3 +1,24 @@
-module.exports = function roman(n) {
-	return 'I';
+var numbers = [ 10,  5,   1 ];
+var letters = ['X', 'V', 'I'];
+
+function roman(n) {
+	var r = '';
+	for(var index = 0; index < numbers.length; index++) {
+		var currentNumber = numbers[index];
+		while(n >= currentNumber) {
+			r = r + letters[index];
+			n -= currentNumber;
+		}
+
+		for(var nextIndex = index + 1; nextIndex <= numbers.length; nextIndex++){
+			var nextNumber = numbers[nextIndex];
+			if(n !== nextNumber && n === currentNumber - nextNumber) {
+				r = letters[nextIndex] + letters[index]  + r;
+				n -= currentNumber - nextNumber;
+			}
+		}
+	}
+	return r;
 }
+
+module.exports = roman;
