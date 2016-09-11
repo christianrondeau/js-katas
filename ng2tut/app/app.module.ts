07c0da9fd1ed10e812afd8fb020b42461ae5be3a
Christian Rondeau <christian.rondeau@gmail.com>
2016-09-11 00:26:02 -0400

    ... Dashboard Top Heroes

--- ng2tut/app/app.module.ts
+++ ng2tut/app/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
+import { routing } from './app.routing';
+
 import { HeroService  }  from './hero.service';
 import { AppComponent }  from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-	imports:      [ BrowserModule, FormsModule ],
+	imports: [ BrowserModule, FormsModule, routing ],
 	providers: [ HeroService ],
-	declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
-	bootstrap:    [ AppComponent ]
+	declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
 
