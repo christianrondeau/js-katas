4b59843dd16f53c52354e13ad99f95747de44c7c
Christian Rondeau <christian.rondeau@gmail.com>
2016-09-12 07:52:49 -0400

    Refactor regex reading

--- pooregex/src/pooregex.js
+++ pooregex/src/pooregex.js
@@ -1,19 +1,31 @@
+function RegexReader(regex) {
+	this.index = -1;
+	this.regex = regex;
+}
+RegexReader.prototype.next = function() {
+	this.token = this.regex[++this.index];
+}
+RegexReader.prototype.isMatchComplete = function() {
+	return this.index == this.regex.length;
+}
+
 function pooregex(s, r) 
 {
 	var m = "";
 	var si = -1;
 	var ri = 0;
-	var t = r[ri];
+	var regexReader = new RegexReader(r);
+	regexReader.next();
 	while(++si < s.length) {
 		var c = s[si];
-		if(c === t) {
+		if(c === regexReader.token) {
 			m += c;
-			t = r[++ri];
+			regexReader.next();
 		} else if(m) {
 			break;
 		}
 	}
-	if(m && ri == r.length) {
+	if(m && regexReader.isMatchComplete()) {
 		return {
 			success: true,
 			matches: [ m ]
