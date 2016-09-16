1d4bcaa1c204d73512adb1651b20362eb63146f1
Christian Rondeau <christian.rondeau@gmail.com>
2016-09-16 23:50:56 +0200

    Extract operators as classes

--- pooregex/src/regex-reader.js
+++ pooregex/src/regex-reader.js
@@ -1,5 +1,29 @@
 "use strict";
 
+class SpecificCharOperator {
+	constructor(token) {
+		this.token = token;
+	}
+
+	isMatch(char) {
+		return char === this.token;
+	}
+}
+
+class AnyCharOperator {
+	constructor(token) {
+		this.token = token;
+	}
+
+	isMatch(char) {
+		return true;
+	}
+}
+
+var operators = {
+	".": AnyCharOperator
+};
+
 class RegexReader {
 	constructor(regex) {
 		this.regex = regex;
@@ -17,10 +41,7 @@ class RegexReader {
 			return;
 		}
 
-		this.token = this.regex[++this.index];
-		if(this.token  === "\\") {
-			this.token = this.regex[++this.index];
-		}
+		this.operator = this.nextOperator();
 
 		var peek = this.regex[this.index + 1];
 
@@ -36,7 +57,20 @@ class RegexReader {
 			this.repeat = 1;
 			this.optional = false;
 		}
-	};
+	}
+	
+	nextOperator() {
+		var token = this.regex[++this.index];
+		if(token  === "\\") {
+			return new SpecificCharOperator(this.regex[++this.index]);
+		}
+		var operatorCtor  = operators[token];
+		if(operatorCtor) {
+			return new operatorCtor();
+		}
+
+		return new SpecificCharOperator(token);
+	}
 
 	isMatchComplete(stringComplete) {
 		var complete = this.index === this.regex.length || (stringComplete && this.index === this.regex.length - 1 && this.repeat < 0);
@@ -44,11 +78,7 @@ class RegexReader {
 	}
 
 	isMatch(char) {
-		if(this.token === ".") {
-			return true;
-		}
-
-		return char === this.token;
+		return this.operator.isMatch(char);
 	}
 
 	doMatch(char) {
